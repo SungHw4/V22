a23f4059ee34b583d612253e9a6d54942fe9a02b
HanYoungIn <900948@naver.com>
2022-10-20 00:56:19 +0900

    update program.cs

--- V22WebServer/V22WebServer/Program.cs
+++ V22WebServer/V22WebServer/Program.cs
@@ -2,6 +2,7 @@ using V22WebServer.Controllers;
 using V22WebServer.MiddleWare;
 using V22WebServer.Service;
 using V22WebServer.Service.Database;
+using V22WebServer.Service.Redis;
 using ZLogger;
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,23 +11,40 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config.SetBasePath(Directory.GetCurrentDirectory());
+        config.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
+    });
+
+builder.Host.ConfigureLogging(logging =>
+{
+    logging.ClearProviders();
+    logging.AddConfiguration(builder.Configuration.GetSection("logging"));
+    logging.AddZLoggerConsole();
+    logging.AddZLoggerFile("ptLog.log");
+    logging.AddZLoggerRollingFile((dt,x) => $"logs/{dt.ToLocalTime():yyyy-MM-dd}_{x:000}.log", x => x.ToLocalTime().Date, 1024);
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
-app.UseHttpsRedirection();
+app.UseRouting();
+app.UserSessionMiddleWare();
 
-app.UseAuthorization();
+//app.UseHttpsRedirection();
 
-app.MapControllers();
+//app.UseAuthorization();
 
+//app.MapControllers();
+
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 Database.Init(builder.Configuration.GetSection("DBConnection")["MySqlGame"]);
-
+Redis.Init(builder.Configuration.GetSection("DBConnection")["Redis"]);
 app.Run();
